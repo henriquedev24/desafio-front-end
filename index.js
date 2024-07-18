0de95b916ad29933b09a5471694ef60c9b347ed8
Gabriel Henrique <henriquedeveloper24@gmail.com>
2024-07-18 19:26:09 -0300

    comitado

--- index.js
+++ index.js
@@ -1,16 +1,3 @@
-function mascara(o, f) {
-    v_obj = o
-    v_fun = f
-    setTimeout("execmascara()", 1)
-}
-
-function execmascara() {
-    v_obj.value = v_fun(v_obj.value)
-}
-
-function mreais(v) {
-    v = v.replace(/\D/g, "") //Remove tudo o que não é dígito
-    v = v.replace(/(\d{2})$/, ",$1") //Coloca a virgula
-    v = v.replace(/(\d+)(\d{3},\d{2})$/g, "$1.$2") //Coloca o primeiro ponto
-    return v
+function mostrarMensagem () {
+    alert('Formulário enviado co sucesso!');
 }
