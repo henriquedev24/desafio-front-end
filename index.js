346d485fa438b6ca35bd9d056b21bb8915cbcd27
Gabriel Henrique <henriquedeveloper24@gamil.com>
2024-07-18 08:24:23 -0300

    adicionei campo preço

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("hello world");
+function mascara(o, f) {
+    v_obj = o
+    v_fun = f
+    setTimeout("execmascara()", 1)
+}
+
+function execmascara() {
+    v_obj.value = v_fun(v_obj.value)
+}
+
+function mreais(v) {
+    v = v.replace(/\D/g, "") //Remove tudo o que não é dígito
+    v = v.replace(/(\d{2})$/, ",$1") //Coloca a virgula
+    v = v.replace(/(\d+)(\d{3},\d{2})$/g, "$1.$2") //Coloca o primeiro ponto
+    return v
+}
